b8624b39efa44e4909e416ff93c0a08b6f99f82a
Daniel Roa rRom <208727102+DecemberCode95@users.noreply.github.com>
2026-03-13 17:31:15 -0500

    Create initial URL configuration for Django project
    Set up URL routing for admin and API authentication endpoints.
    
    Signed-off-by: Daniel Roa rRom <208727102+DecemberCode95@users.noreply.github.com>

---
+++ config/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls)),
+    path('api/shop/', include('shop.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
